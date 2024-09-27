d6d74dbc21af336b87947e3c28f6775b945c3661
I Dewa Gede Ngr Bramasta D <68767@fif.co.id>
2024-09-27 15:03:30 +0700

    Story 2

--- exercise/day-1/Belajar-Kotlin-Day1/src/main/kotlin/Main.kt
+++ exercise/day-1/Belajar-Kotlin-Day1/src/main/kotlin/Main.kt
@@ -6,15 +6,25 @@ import java.util.*
 
 
 fun main() {
-    println("Hello World")
-    var num = Scanner(System.`in`)
-    var score = num.nextInt()
-    when(score){
-        in 90..100 -> print("A")
-        in 80..89 -> print("B")
-        in 70..79 -> print("C")
-        in 61..69 -> print("D")
-        in 0..60 -> print("F")
-        !in 0..100 -> print("Invalid")
+//    println("Hello World")
+//    var num = Scanner(System.`in`)
+//    var score = num.nextInt()
+//    when(score){
+//        in 90..100 -> print("A")
+//        in 80..89 -> print("B")
+//        in 70..79 -> print("C")
+//        in 61..69 -> print("D")
+//        in 0..60 -> print("F")
+//        !in 0..100 -> print("Invalid")
+//    }
+    print("Salary : ")
+    var salary = Scanner(System.`in`).nextDouble()
+    print("Years Of Service : ")
+    var yearsOfService = Scanner(System.`in`).nextInt()
+    var bonus: Double = when(yearsOfService){
+        in 5..10 -> salary * 0.2
+        in 2..5 -> salary * 0.1
+        else -> 0.0
     }
+    println("Bonus : " +bonus)
 }
