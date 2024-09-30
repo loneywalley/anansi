3fda690f8fcfea869ff85d8a2e61902b4611656f
I Dewa Gede Ngr Bramasta D <68767@fif.co.id>
2024-09-30 17:16:15 +0700

    perubahan day2

--- exercise/day-2/Day2/src/main/kotlin/ProjectManager.kt
+++ exercise/day-2/Day2/src/main/kotlin/ProjectManager.kt
@@ -4,6 +4,7 @@ class ProjectManager {
     val freelancers = mutableListOf<Freelancer>()
     val projects = mutableListOf<Project>()
 
+    //Add new freelancer
     fun addFreelancer(name: String, hourlyRate: Double) {
         val freelancer = Freelancer(
             id = freelancers.size + 1,
@@ -11,8 +12,10 @@ class ProjectManager {
             hourlyRate = hourlyRate
         )
         freelancers.add(freelancer)
+        println("Freelancer Added Successfully!")
     }
 
+    //Create new project
     fun createProject(name: String, client: String, deadline: LocalDate) {
         val project = Project(
             id = projects.size + 1,
@@ -23,14 +26,14 @@ class ProjectManager {
             totalBudget = 0.0
         )
         projects.add(project)
+        println("Project Created Successfully!")
     }
 
+    //Assign task to user
     fun assignTask(projectId: Int, freelancerId: Int, description: String, estimatedHours: Int) {
         val project = projects.find { it.id == projectId }
         val freelancer = freelancers.find { it.id == freelancerId }
         if (project != null && freelancer != null) {
-            require(description.isNotEmpty()) { "Task description cannot be empty" }
-            require(estimatedHours > 0) { "Estimated hours must be positive" }
 
             val task = Task(
                 id = project.tasks.size + 1,
@@ -40,26 +43,38 @@ class ProjectManager {
             )
             project.tasks.add(task)
             freelancer.assignedTasks.add(task)
+            println("Task Assigned Successfully!")
         } else {
-            println("Project or freelancer not found!")
+            throw Exception ("Project or freelancer not found!")
         }
     }
 
+    //Log Hours Worked
     fun logHoursWorked(taskId: Int, hoursWorked: Int) {
         val task = projects.flatMap { it.tasks }.find { it.id == taskId }
         if (task != null) {
-            task.actualHours += hoursWorked
-            if (task.actualHours > task.estimatedHours * 1.5) {
-                println("Warning: Actual hours exceed estimated hours by more than 50%")
+            // Check if task was completed
+            if (task.status != "Completed") {
+                task.actualHours += hoursWorked
+                if (task.actualHours > task.estimatedHours * 1.5) {
+                    throw Exception("Warning: Actual hours exceed estimated hours by more than 50%")
+                }
+                updateTaskStatus(task)
+                val projectId = projects.flatMap { it.tasks }.find { it.id == taskId }
+                    ?.let { task -> projects.find { it.tasks.contains(task) }?.id }
+                if (projectId != null) {
+                    calculateProjectBudget(projectId)
+                }
+                println("Hours Worked Logged Successfully!")
+            } else {
+                throw Exception("Project already finished")
             }
-            updateTaskStatus(task)
-            val projectId = projects.flatMap { it.tasks }.find { it.id == taskId }?.let { task -> projects.find { it.tasks.contains(task) }?.id }
-            if (projectId != null) {
-                calculateProjectBudget(projectId)
-            }
+        }  else {
+            throw Exception("Task not found!")
         }
     }
 
+    //Update Task Status
     fun updateTaskStatus(task: Task) {
         if (task.actualHours >= task.estimatedHours) {
             task.status = "Completed"
@@ -68,6 +83,7 @@ class ProjectManager {
         }
     }
 
+    //Calculate project budget by Id
     fun calculateProjectBudget(projectId: Int) {
         val project = projects.find { it.id == projectId }
         project?.let {
@@ -78,6 +94,7 @@ class ProjectManager {
         }
     }
 
+    //View project Detais by Id
     fun viewProjectDetails(projectId: Int) {
         val project = projects.find { it.id == projectId }
         if (project != null) {
@@ -87,14 +104,13 @@ class ProjectManager {
             println("Tasks:")
             project.tasks.forEach {
                 println("ID : ${it.id}")
+                println("Freelancer : ${it.freelancer.name}")
                 println("Status : ${it.status}")
                 println("Description : ${it.description}")
                 println("Actual Hours : ${it.actualHours}")
                 println("Estimate Hours : ${it.estimatedHours}") }
             calculateProjectBudget(projectId)
             println("Total Budget: ${project.totalBudget}")
-        }else{
-            throw Exception("Please Enter Project Id")
         }
     }
 }
